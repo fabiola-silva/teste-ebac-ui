d024506ea15d6acf7f476e70044868d2186f3bc0
fabiola <fabiolasilvaoffice@gmail.com>
2022-07-26 16:17:15 -0400

    Criando massa de dados estaticamente

--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -1,5 +1,6 @@
 /// <reference types= "cypress"/>
 import EnderecoPage from '../support/page-objects/endereco.page'
+const dadosEnderecos = require('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
@@ -10,8 +11,26 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
         
     });
     
-    it.only('Deve fazer cadastro de faturamento com sucesso', () => {
+    it('Deve fazer cadastro de faturamento com sucesso', () => {
         EnderecoPage.editarEnderecoFasturamento('Noah', 'Vasconcelos', 'TecInfo', 'Australia', 'Adelaide', '5555', 'Melbourme', 'Queensland', '658799988', '11985782145', 'tecinfo@teste.com')
+        cy.get('.woocommerce-message').should('contain' , 'Endereço alterado com sucesso.')
+        
+    });
+
+    it.only('Deve fazer cadastro de faturamento com sucesso - Usando arquivo de dados', () => {
+        EnderecoPage.editarEnderecoFasturamento(
+            dadosEnderecos[0].nome,
+            dadosEnderecos[0].sobrenome,
+            dadosEnderecos[0].empresa,
+            dadosEnderecos[0].pais,
+            dadosEnderecos[0].endereco,
+            dadosEnderecos[0].numero,
+            dadosEnderecos[0].cidade,
+            dadosEnderecos[0].estado,
+            dadosEnderecos[0].cep,
+            dadosEnderecos[0].telefone,
+            dadosEnderecos[0].email
+            )
         cy.get('.woocommerce-message').should('contain' , 'Endereço alterado com sucesso.')
         
     });
--- cypress/support/page-objects/endereco.page.js
+++ cypress/support/page-objects/endereco.page.js
@@ -13,10 +13,9 @@ class EnderecoPage {
         cy.get('#billing_city').clear().type(cidade)
         cy.get('#select2-billing_state-container').click().type(estado + '{enter}')
         cy.get('#billing_postcode').clear().type(cep)
-        cy.get('#billing_postcode').clear().type(telefone)
+        cy.get('#billing_phone').clear().type(telefone)
         cy.get('#billing_email').clear().type(email)
-        cy.get('.button').click()
-
+        cy.get(':nth-child(2) > .button').click()
     }
     
     editarEnderecoEntrega(){
